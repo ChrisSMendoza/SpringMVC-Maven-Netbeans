9d89c0c108a301b8ffdf2bf3b86fa2fc304fed1c
Chris Mendoza <realcsmendoza@gmail.com>
2019-05-16 23:32:03 -0700

    integration test folder added with example success test

---
+++ src/test/integration/SuccessIT.java
@@ -0,0 +1,13 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class SuccessIT {
+    
+    // dummy test to ensure it runs separately from unit tests
+    @Test
+    public void successfulTest() {
+        assertEquals(true, true);
+    }
+}
